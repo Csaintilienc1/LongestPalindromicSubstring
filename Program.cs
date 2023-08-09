45ddcce22753b894912b819d31f09b25ce604d02
Csaintilien <saintilienclaudel@gmail.com>
2023-08-09 16:41:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LongestPalindromicSubstring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            longestPalindromicSubstring test = new();
+
+            Console.WriteLine(test.longestPalSubstring("abbabbaaba"));
+
+        }
+    }
+}
---
+++ longestPalindromicSubstring.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestPalindromicSubstring
+{
+    public class longestPalindromicSubstring
+    {
+        public string longestPalSubstring(string a)
+        {
+
+            char[] charArray = a.ToCharArray();
+
+            int begin = 0;
+            
+            int end = charArray.Length-1;
+
+            String longestPalSubstr = " ";
+            while(begin < end)
+            {
+                if (charArray[begin] == charArray[end])
+                {
+                    string currentPalSubstr = a.Substring(begin, end);
+                    if(currentPalSubstr.Length > longestPalSubstr.Length) 
+                    {
+                        longestPalSubstr = currentPalSubstr;
+                        begin++;
+                        end--;
+                    
+                    }
+                    else
+                    {
+                        begin++;
+                        end--;
+
+                    }
+                   // return longestPalSubstr;
+                   // begin++;
+                   // end--;
+                }
+              //   return longestPalSubstr;
+              //  begin++;
+              //  end--;
+            }
+            return longestPalSubstr;
+        }
+    }
+}
